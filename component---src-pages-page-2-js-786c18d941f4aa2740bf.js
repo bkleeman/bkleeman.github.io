36ac30842fa3ee6749c4b188a295e8b63352ce2a
bkleeman <benjamin.kleeman@gmail.com>
2020-04-04 15:45:02 -0500

    add new chronoblog site

--- component---src-pages-page-2-js-786c18d941f4aa2740bf.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{207:function(e,t,a){"use strict";a.r(t);var n=a(0),o=a.n(n),c=a(100),l=a(203);t.default=function(){return o.a.createElement(l.a,null,o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(c.a,{to:"/"},"Go back to the homepage"))}}}]);
-//# sourceMappingURL=component---src-pages-page-2-js-786c18d941f4aa2740bf.js.map
